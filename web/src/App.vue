ea4a9e7ab4db50fc97f6c3b86edeac1625dde33b
TonyzzZ666 <1980292554@qq.com>
2026-07-11 11:15:40 +0800

    v2.4.1: Apple风格登录过渡动画/页面切换动画

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="app-switch" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style>
@@ -13,8 +16,20 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
+}
+
+.app-switch-enter-active {
+  transition: all 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+}
+.app-switch-leave-active {
+  transition: all 0.25s ease-out;
+}
+.app-switch-enter-from {
+  opacity: 0;
+  transform: scale(1.03);
+}
+.app-switch-leave-to {
+  opacity: 0;
+  transform: scale(0.98);
 }
 </style>
--- web/src/layout/modules/Main.vue
+++ web/src/layout/modules/Main.vue
@@ -1,9 +1,26 @@
 <template>
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="page-fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+.page-fade-enter-active {
+  transition: all 0.35s cubic-bezier(0.4, 0, 0.2, 1);
+}
+.page-fade-leave-active {
+  transition: all 0.2s ease-in;
+}
+.page-fade-enter-from {
+  opacity: 0;
+  transform: scale(0.97) translateY(8px);
+}
+.page-fade-leave-to {
+  opacity: 0;
+}
 </style>
